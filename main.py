17e4f078b43bc39ba8da7df1472f0f072bc54273
Anandu G <anandhuabhi112@gmail.com>
2026-05-23 18:17:28 +0530

    Update CORS middleware to allow all methods

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = FastAPI(title="Word Puzzle API", version="2.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
